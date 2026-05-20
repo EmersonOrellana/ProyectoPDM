5824beb501c73adebe19b8aefd089f7556b9f4d3
RemberV15 <remberto.chavez02@gmail.com>
2026-05-19 23:04:21 -0600

    actualizar id de barra de navegación en MainActivity

--- app/src/main/java/com/example/proyectopdm/LoginFragment.kt
+++ app/src/main/java/com/example/proyectopdm/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.button.MaterialButton
 
@@ -13,20 +14,22 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Enlazamos este código con el diseño XML
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Buscamos el botón de ingresar
         val btnIngresar = view.findViewById<MaterialButton>(R.id.btn_ingresar)
 
         btnIngresar.setOnClickListener {
-            // Cuando hacemos clic, le decimos a la Actividad Principal que cambie a la pantalla de Inicio
+            // === MODO PRUEBA: PASAMOS DIRECTO AL INICIO ===
+
             val mainActivity = activity as MainActivity
             mainActivity.cambiarPantalla(InicioFragment(), R.id.nav_inicio, "COTMAN")
+
+            // Un pequeño mensaje flotante de bienvenida
+            Toast.makeText(requireContext(), "¡Bienvenido a COTMAN!", Toast.LENGTH_SHORT).show()
         }
     }
 }
