6f9b5d04b73f4c3afc469453611573a12fbcc3b6
EmersonOrellana <ot20006@ues.edu.sv>
2026-05-21 18:29:37 -0600

    reparacion del perfil y conexion

--- app/src/main/java/com/example/proyectopdm/PerfilFragment.kt
+++ app/src/main/java/com/example/proyectopdm/PerfilFragment.kt
@@ -4,21 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.card.MaterialCardView
 
 class PerfilFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Asegúrate de que fragment_perfil exista en res/layout
         return inflater.inflate(R.layout.fragment_perfil, container, false)
     }
-
-
 }
